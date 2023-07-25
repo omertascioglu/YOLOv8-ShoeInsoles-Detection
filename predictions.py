534a3b3dcc3fb2b99730a19ea7e0cf4c39766605
Ömer Taşçıoğlu <33811400+omertascioglu@users.noreply.github.com>
2023-07-25 09:22:15 +0300

    Update predictions.py

--- predictions.py
+++ predictions.py
@@ -5,5 +5,5 @@ import cv2
 model = YOLO("best.pt")
 
 
-results = model.predict(source="0", show=True) # accepts all formats - img/folder/vid. *(i
+results = model.predict(source="0", show=True)
 print(results)
