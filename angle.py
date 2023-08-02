01dc808f5ea42fbbbf35ccbd7bada5808bbd754d
Ömer Taşçıoğlu <33811400+omertascioglu@users.noreply.github.com>
2023-08-02 21:52:02 +0300

    Add files via upload
    Thanks to this file, we are taking our project one step further. In addition to the shoe sole, we can now estimate the angles.

---
+++ angle.py
@@ -0,0 +1,64 @@
+import cv2
+import numpy as np
+from ultralytics import YOLO
+from ultralytics.yolo.v8.detect.predict import DetectionPredictor
+from scipy import ndimage
+import math
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+# model
+model = YOLO("best.pt")
+
+
+def calculate_angle(x1, y1, x2, y2):
+    # Calculate the angle between the line and the x-axis
+    angle_rad = np.arctan2(y2 - y1, x2 - x1)
+    angle_deg = np.degrees(angle_rad)
+    return angle_deg
+
+while True:
+    cap = cv2.VideoCapture(0)
+    ret, frame = cap.read()
+    results = model(frame, stream=True, conf=0.75)
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray, 50, 150, apertureSize=3)
+    lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi / 180, threshold=100, minLineLength=100, maxLineGap=5)
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line[0]
+            cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+            angle = calculate_angle(x1, y1, x2, y2)
+            cv2.putText(frame, f"Angle: {angle:.2f} degrees", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            print(angle)
+    # coordinates
+    for r in results:
+        boxes = r.boxes
+
+        for box in boxes:
+            # bounding box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
+
+            # put box in cam
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (243, 239, 224), 3)
+
+            # object details
+            org = [x1, y1]
+            font = cv2.FONT_HERSHEY_COMPLEX
+            fontScale = 1
+            color = (255, 0, 0)
+            thickness = 2
+
+            cv2.putText(frame, """Ayakkabi Tabani""", org, font, fontScale, color, thickness)
+
+    cv2.imshow('Webcam', frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
